573be2166da0bba746e2895d91e1b0279d4642c3
mhchoi09 <32935256+mhchoi09@users.noreply.github.com>
2023-07-01 23:40:09 +0800

    Changes to the AuthenticatorApp Class

--- AuthenticatorApp.cs
+++ AuthenticatorApp.cs
@@ -1,24 +1,34 @@
-﻿string[] userNames = { "mingyu", "seokmin", "minghao" };
-string[] password = { "runningman7012", "yeheyyehey", "dontcare" };
-
-Console.Write("userName:");
-var currentUser = Console.ReadLine();
-Console.Write("password:");
-var currentPassword = Console.ReadLine();
-
-bool isUserFound = userNames.Contains(currentUser);
-
-for (int i = 0; i <= userNames.Length; i++)
+﻿namespace CodingChallengeTutorial
 {
-    if (userNames[i] == currentUser && password[i] == currentPassword)
+    public class AuthenticatorApp
     {
-        isUserFound = true;
-        Console.Write($"Login is successful. Welcome {currentUser}");
-        break;
-    }
-}
 
-if (!isUserFound)
-{
-    Console.Write($"User \"{currentUser}\" not found");
+        public static void NewMethod()
+        {
+            string[] userNames = { "mingyu", "seokmin", "minghao" };
+            string[] password = { "runningman7012", "yeheyyehey", "dontcare" };
+
+            Console.Write("userName:");
+            var currentUser = Console.ReadLine();
+            Console.Write("password:");
+            var currentPassword = Console.ReadLine();
+
+            bool isUserFound = userNames.Contains(currentUser);
+
+            for (int i = 0; i <= userNames.Length; i++)
+            {
+                if (userNames[i] == currentUser && password[i] == currentPassword)
+                {
+                    isUserFound = true;
+                    Console.Write($"Login is successful. Welcome {currentUser}");
+                    break;
+                }
+            }
+
+            if (!isUserFound)
+            {
+                Console.Write($"User \"{currentUser}\" not found");
+            }
+        }
+    }
 }
